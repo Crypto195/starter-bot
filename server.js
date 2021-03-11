23e80b959b5af388fd586a4ddf30f2e4894c59fd
Crypto <73986063+Crypto195@users.noreply.github.com>
2021-03-11 11:51:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const { prefix } = require("./config.json");
 const { token } = require("./config.json");
 const { config } = require("dotenv");
-
 const discord = require("discord.js");
 const client = new discord.Client({
   disableEveryone: false
 });
+
+client.queue = new Map();
+client.vote = new Map();
+const { ready } = require("./handlers/ready.js")
+
+
 
 client.commands = new discord.Collection();
 client.aliases = new discord.Collection();
 
 ["command"].forEach(handler => {
   require(`./handlers/${handler}`)(client);
-});
-
-client.on("ready", () => {
-  console.log(` ${client.user.username} is turned on`);
-  client.user.setActivity(`${prefix}help`,{type:"STREAMING"});
 });
 
 client.on("message", async message => {
@@ -47,5 +47,14 @@ client.on("message", async message => {
   if (command) command.run(client, message, args);
 });
 
+// Set the bot's online/idle/dnd/invisible status
+client.on("ready", () => {
+    client.user.setStatus("online");
+    console.log("The bot is online")
+});
 
-client.login(token)
+
+client.on("ready", () => {
+    client.user.setActivity(`${prefix}help in ${client.guilds.cache.size} servers`, { type: "WATCHING"})
+})
+client.login(token);
